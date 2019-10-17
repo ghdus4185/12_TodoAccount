8ca13caf7e09318f0e9ab9a20990e54d9a5a32d9
ghdus4185 <ckdghdus@naver.com>
2019-10-17 10:02:52 +0900

    signup 완성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # 자동으로 로그인 시켜주고
+            # 인덱스 페이지로 redirect 시켜준다.
+            auth_login(request, user)
+            return redirect('todos:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
--- wunderlist/settings.py
+++ wunderlist/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'wunderlist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'wunderlist', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
